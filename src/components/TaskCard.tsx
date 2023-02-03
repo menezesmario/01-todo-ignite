9f8309b0fb6bdf459c84b04685dbc2d1f5da580a
Mario Menezes <mario082@gmail.com>
2023-02-03 09:39:55 -0300

    install tailwind

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,7 +1,7 @@
 export function TaskCard() {
   return (
-    <div>
-      <div>
+    <div className="bg-slate-500 rounded flex justify-between">
+      <div className="flex gap-2">
         <div>checked</div>
         <div>Task name</div>
       </div>
